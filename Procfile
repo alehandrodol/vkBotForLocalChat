1409322d2e2b3b2224ef6d8e06764df7c5b9a4bc
Алексей Долгих <alexandrodol2002@gmail.com>
2022-01-17 07:18:59 +0300

    test

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 worker: python main.py $PORT
+vote: python vote_waiting.py
---
+++ vote_waiting.py
@@ -0,0 +1,18 @@
+import vk_api
+import os
+
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType, VkBotMessageEvent
+
+
+def listen():
+    vk_session = vk_api.VkApi(
+        token=os.environ.get("GROUP_TOKEN"))
+    vk = vk_session.get_api()
+    params = vk.groups.getLongPollServer(group_id=209871225)
+
+    longpoll = VkBotLongPoll(vk_session, 209871225)
+
+    for event in longpoll.listen():
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            if event.message['text'].lower() == "тест":
+                print("найс")

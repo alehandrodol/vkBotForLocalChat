14038051bbd1767a17fed247752fc8fb12f1b3ba
alehandrodol <alexeydol2002@mail.ru>
2022-02-03 20:07:36 +0300

    hot fix

--- main.py
+++ main.py
@@ -657,6 +657,8 @@ class Bot:
         if record_user_achieve is None:
             record_user_achieve = self.make_empty_record_in_users_achieves(event=event, db=db, user_id=for_user, achieve_id=7)
 
+        commit(record_user_achieve)
+
         moscow_zone = pytz.timezone("Europe/Moscow")
         now = datetime.now(tz=moscow_zone)
         if record_user_achieve.is_got and (record_user_achieve.reachieve_date is None or record_user_achieve.reachieve_date > now.date()):

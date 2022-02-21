8dbe5c8c5fce218b21a1448ee207522aa8e13ab2
alehandrodol <alexeydol2002@mail.ru>
2022-02-21 18:46:10 +0300

    hot fix

--- models.py
+++ models.py
@@ -19,13 +19,13 @@ class Group(Base):
     for_user_vote = Column(Integer)
 
     def __str__(self) -> str:
-        return f"--------------------------------------------------------------------------------------------------\n" \
+        return f"\n------------------------------------------------------------------------------------------------\n" \
                f"Instanse from table groups:\n" \
                f"id: {self.id}; name: {self.name}, today_pdr: {self.today_pdr}, who_is_fucked: {self.who_is_fucked}, " \
                f"pdr_date: {self.pdr_date},\nyear_pdr: {self.year_pdr}, year_pdr_num: {self.year_pdr_num},\n" \
                f"active_vote: {self.active_vote}, start_time: {self.start_time}, votes_counter: {self.votes_counter}, "\
                f"for_user_votes: {self.for_user_vote}.\n" \
-               f"--------------------------------------------------------------------------------------------------\n"
+               f"------------------------------------------------------------------------------------------------\n"
 
 
 class User(Base):
@@ -42,12 +42,12 @@ class User(Base):
     group = relationship(Group)
 
     def __str__(self) -> str:
-        return f"--------------------------------------------------------------------------------------------------\n" \
+        return f"\n------------------------------------------------------------------------------------------------\n" \
                f"Instanse from table users:\n" \
                f"id: {self.id}, chat_id: {self.chat_id}, firstname: {self.firstname}, lastname: {self.lastname},\n" \
                f"pdr_num: {self.pdr_num}, fucked: {self.fucked}, rating: {self.rating}, " \
                f"pdr_of_the_year: {self.pdr_of_the_year}.\n" \
-               f"--------------------------------------------------------------------------------------------------\n"
+               f"------------------------------------------------------------------------------------------------\n"
 
 
 class Achieves(Base):
@@ -61,12 +61,12 @@ class Achieves(Base):
     secs_to_reseting = Column(Integer)
 
     def __str__(self) -> str:
-        return f"--------------------------------------------------------------------------------------------------\n" \
+        return f"\n------------------------------------------------------------------------------------------------\n" \
                f"Instance from table achieves:\n" \
                f"id: {self.id}, name: {self.name}, points: {self.points}, "\
                f"needed_repeats: {self.needed_repeats}, day_count_reachieve: {self.day_count_reachieve}," \
                f"secs_to_reseting: {self.secs_to_reseting}.\n" \
-               f"--------------------------------------------------------------------------------------------------\n"
+               f"------------------------------------------------------------------------------------------------\n"
 
 
 class UserAchieve(Base):
@@ -82,12 +82,12 @@ class UserAchieve(Base):
     got_times = Column(Integer)
 
     def __str__(self) -> str:
-        return f"--------------------------------------------------------------------------------------------------\n" \
+        return f"\n------------------------------------------------------------------------------------------------\n" \
                f"Instance from table users_achieves:\n" \
                f"user_id: {self.user_id}, achieve_id: {self.achieve_id}, last_date: {self.last_date},\n" \
                f"current_repeats: {self.current_repeats}, is_got: {self.is_got}, \n" \
                f"reachieve_date: {self.reachieve_date}, got_times: {self.got_times}.\n" \
-               f"--------------------------------------------------------------------------------------------------\n"
+               f"------------------------------------------------------------------------------------------------\n"
 
 
 class GroupAchieve(Base):
@@ -98,7 +98,7 @@ class GroupAchieve(Base):
     is_available = Column(Boolean)
 
     def __str__(self) -> str:
-        return f"--------------------------------------------------------------------------------------------------\n" \
+        return f"\n------------------------------------------------------------------------------------------------\n" \
                f"Instance from table groups_achieves:\n" \
                f"groups_id: {self.group_id}, achieve_id: {self.achieve_id}, is_available: {self.is_available}" \
-               f"--------------------------------------------------------------------------------------------------\n"
+               f"------------------------------------------------------------------------------------------------\n"
